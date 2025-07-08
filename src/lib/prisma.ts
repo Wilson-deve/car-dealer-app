8f95dada96f233506ed314cfbace0e767898adc9
Wilson-deve <nshimiyimanawilson91@gmail.com>
2025-07-08 23:03:55 +0200

    model setup in prisma

---
+++ src/lib/prisma.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+function makeClient() {
+  return new PrismaClient({
+    log: ["query", "info", "warn", "error"],
+  });
+}
+
+export const prisma = globalThis.prisma || makeClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma;
+}
+
+process.on("beforeExit", async () => {
+  await prisma.$disconnect();
+});
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
+process.on("SIGTERM", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
